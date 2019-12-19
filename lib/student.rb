660f09f8c6804adca50104c722fe9299eccb363d
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-12-19 04:40:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,8 @@ class Student
   def self.new_from_db(row)
    student = self.new
    student.id = row[0]
-   student.name =  row[1]
-   student.length = row[2]
+   student.name = row[1]
+   student.grade = row[2]
    student
   end
 
