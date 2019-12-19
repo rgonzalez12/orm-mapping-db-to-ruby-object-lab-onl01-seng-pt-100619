f68f19c4cf85d19574f35007e9ff4a04f77323cd
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-12-19 04:49:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,6 +71,6 @@ class Student
   end
   
   def self.first_x_students_in_grade_10
-    sql = "SELECT * FROM students WHERE grade = 10 "
+    sql = "SELECT * FROM students WHERE grade = 10 ORDER BY students.id "
   end
 end
