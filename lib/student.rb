39ff94fa217750d174624955225b3ff91ac5dc7b
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-12-19 04:58:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,4 +79,8 @@ class Student
   
   def self.first_student_in_grade_10
     sql = "SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT 1;"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
